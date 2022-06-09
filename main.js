a6e2a45ebe8cee0ce87fe723b19091a362e8d67c
Surgetin <65120855+Surgetin@users.noreply.github.com>
2022-06-09 15:52:58 +0200

    added responsive menu

--- main.js
+++ main.js
@@ -14,3 +14,23 @@ tabs.forEach(tab => {
     target.classList.add('active')
   })
 })
+
+
+const toggleMenu = document.querySelector(".menu");
+const navItem = document.querySelector(".nav__list-items");
+const navbar = document.querySelector(".header");
+
+toggleMenu.addEventListener("click", () => {
+    toggleMenu.classList.toggle("active");
+    navItem.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav__list-link").forEach(e => 
+    e.addEventListener("click", () => {
+        toggleMenu.classList.remove("active");
+        navItem.classList.remove("active");
+}));
+
+/* window.addEventListener("scroll", () => {
+    navbar.classList.toggle("nav-srolled", window.scrollY > 0);
+}) */
